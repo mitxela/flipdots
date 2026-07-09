13b7d31a708d9bd0ba22a10704573745b052b6a4
mitxela <mitxela@mitxela.com>
2026-07-09 23:00:57 +0100

    stream full display

--- s9.py
+++ s9.py
@@ -1,10 +1,15 @@
 from Xlib import display, X
 from PIL import Image, ImageDraw, ImageOps
-import serial, time
+import sys, serial, time
+
+if len(sys.argv) == 3:
+	fullscreen = 1 sys.argv[1]=='1' else 0
+	dither = 0 sys.argv[2]=='1' else 0
+else:
+	print("Usage: ",sys.argv[0],"fullscreen dither")
+	exit()
 
 totalFrames = 8
-
-scale=1
 
 w=28*2
 h=26*2
@@ -12,16 +17,28 @@ h=26*2
 d = display.Display()
 root = d.screen().root
 
+dw = root.get_geometry().width
+dh = root.get_geometry().height
+
+sw=w
+sh=h
+offx=0
+if fullscreen:
+	sw = (int)(w*(dh/h))
+	sh=dh
+	offx = (int)((dw-sw)/2)
+
+
 
 def grab_bytes():
-	raw = root.get_image(0,0,w,h, X.ZPixmap, 0xffffffff)
-	image = Image.frombytes("RGB", (w, h), raw.data, "raw", "BGRX")
+	raw = root.get_image(offx,0,sw,sh, X.ZPixmap, 0xffffffff)
+	image = Image.frombytes("RGB", (sw, sh), raw.data, "raw", "BGRX")
 	image = ImageOps.invert(image)
 	#image.show()
 	image = (image.resize((w,h))
 		.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
 		.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
-		.convert("1", dither=1))
+		.convert("1", dither=dither))
 	return image.tobytes()
 
 
